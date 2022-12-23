54c48d3530f4575bb5c3d39f4444c241febffdf0
Migueldar <mdearpe@ucm.es>
2022-12-23 02:56:42 +0100

    initial commit

---
+++ bcd.sh
@@ -0,0 +1,26 @@
+currentDots=0
+cdTo=""
+allDots=true
+temp=""
+
+for ((i=0; i<${#1}+1; i++)); do
+	temp+=${1:$i:1}
+	if [ "." == "${1:$i:1}" ]; then
+		currentDots=$((currentDots+1))
+	elif [ "/" == "${1:$i:1}" ] || [ $i -eq ${#1} ]; then
+		if [ $i -eq 0 ] || ! $allDots; then
+			cdTo+=$temp
+		else
+			for ((j=1; j<${currentDots}; j++)); do
+				cdTo+=../
+			done
+		fi
+		allDots=true
+		currentDots=0
+		temp=""
+	else
+		allDots=false
+	fi
+done
+
+cd $cdTo
